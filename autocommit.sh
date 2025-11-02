324d0df63eb19d82a74bfe06ea9a094211624b4c
Supply12 <arseniyshtaub@gmail.com>
2025-11-02 14:23:04 +0300

    02.11.25 at

--- autocommit.sh
+++ autocommit.sh
@@ -1,1 +1,6 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+git add .
+git commit -m "$(date +%d.%m.%y) at $(date +%t)"
+git branch -M main
+git remote add origin https://github.com/Supply12/mrcoolcola-nix-dots.git
+git push -u origin main
